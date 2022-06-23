2a20367d884847b5842ab673418a2ad17e1c5ea6
wyu1942 <108049221+wyu1942@users.noreply.github.com>
2022-06-22 22:55:07 -0700

    Add files via upload

---
+++ NumericData.py
@@ -0,0 +1,21 @@
+print("Python has three numeric types: int, float, and complex")
+myvalue = 1
+print(myvalue)
+print(type (myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+myvalue = 3.14
+print(myvalue)
+print(type (myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+myvalue = 5j
+print(myvalue)
+print(type (myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+myvalue=True
+print(myvalue)
+print(type (myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
+myvalue=False
+print(myvalue)
+print(type (myvalue))
+print(str(myvalue) + " is of the data type " + str(type(myvalue)))
